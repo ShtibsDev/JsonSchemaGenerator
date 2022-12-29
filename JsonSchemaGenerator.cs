4c29cc5e38be9d1d99cb8cfef540363af01710f7
Ofir Stiber Voronzov <ofir.stiber@ideo-digital.com>
2022-12-29 17:24:23 +0200

    first commit

---
+++ JsonSchemaGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Serialization;
+using NJsonSchema;
+using NJsonSchema.Generation;
+
+namespace SchemaGenerator;
+public static class JsonSchemaGenerator
+{
+	public static string GenerateSchema<T>()
+	{
+		var contractResolver = new DefaultContractResolver { NamingStrategy = new CamelCaseNamingStrategy() };
+		var settings = new JsonSchemaGeneratorSettings { SerializerSettings = new() { ContractResolver = contractResolver } };
+		var schema = JsonSchema.FromType<T>(settings);
+		return schema.ToJson();
+	}
+}
